67ab9309cfb55ae72f6f515d688e23ef87835b25
JeffChou <registry.chou@gmail.com>
2015-05-17 01:00:15 +0800

    add emotion logic(brief).

--- AnalyseCenter.ino
+++ AnalyseCenter.ino
@@ -1,8 +1,21 @@
 
 #include <TimerOne.h>
-float temprature;
+float temperature;
 int emotion =0;
 int hbStr=0;
+const int TEMP_MIN = 32;
+const int TEMP_MID = 35;
+const int TEMP_MAX = 38;
+
+const int AMP_MIN = 0;
+const int AMP_MID = 0;
+const int AMP_MAX = 0;
+
+const int BPM_MIN = 0;
+const int BPM_MID = 0;
+const int BPM_MAX = 0;
+
+int result = 0;
 
 void startAnalyse(){
   Timer1.initialize(2000);
@@ -11,13 +24,47 @@ void startAnalyse(){
 
 void Analyse(){
 HeartBeatAlg();
-temprature = measureObjectTemp();
+temperature = measureObjectTemp();
 //Temperature
 //analyse
+
+// Normal
+if ((temperature >= TEMP_MIN) && (temperature < TEMP_MID) && (BPM >= BPM_MIN) && (BPM < BPM_MID) && (amp >= AMP_MIN) && (amp < AMP_MID))
+{
+  result = 1;
+}
+else if ((temperature >= TEMP_MIN) && (temperature < TEMP_MID) && (BPM >= BPM_MIN) && (BPM < BPM_MID) && (amp >= AMP_MID) && (amp < AMP_MAX))
+{
+
+}
+else if ((temperature >= TEMP_MIN) && (temperature < TEMP_MID) && (BPM >= BPM_MIN) && (BPM < BPM_MID) && (amp >= AMP_MAX))
+{
+
+}
+else
+  result = NOUN;
+
+// result
+switch (result) {
+  case 0:
+    emotion = LOWEMO;
+    break;
+  case 1:
+    emotion = NORMAL;
+    break;
+  case 2:
+    emotion = HIGHEMO;
+    break;
+  default:
+    emotion = NOUN;
+}
 
 //send
 int herb1=hbStr*1000+BPM;
 sendDataToRemoate("heartb1", herb1);
+Serial.print("*** AMP Happened *** ");
+Serial.println(BPM);
+
 sendDataToRemoate("emot1", emotion) ;
 }
 
